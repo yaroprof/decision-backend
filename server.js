b48ed701c9be872681099e5868b16c400eea6a24
Yaroslav Prof <yaroslavsprof@gmail.com>
2025-05-29 13:49:11 +0300

    final-1.0

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ import messageRoutes from './routes/messageRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://decision-frontend-gamma.vercel.app/',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
